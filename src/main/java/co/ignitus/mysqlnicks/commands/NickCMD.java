a2a29c2fc01d87d18a5edadcc4ca098b79342c59
DDarkInferno <mcddark@gmail.com>
2021-01-07 19:38:53 +1100

    Update 1.0.7 - Console can now use /nickname (player) (nickname/off)

--- src/main/java/co/ignitus/mysqlnicks/commands/NickCMD.java
+++ src/main/java/co/ignitus/mysqlnicks/commands/NickCMD.java
@@ -21,28 +21,28 @@ public class NickCMD implements CommandExecutor {
             sender.sendMessage(MessageUtil.getMessage("nick.insufficient-arguments"));
             return true;
         }
-        if (!(sender instanceof Player)) {
-            sender.sendMessage(MessageUtil.getMessage("nick.no-console"));
-            return true;
-        }
-        final Player player = (Player) sender;
-        if (!player.hasPermission("mysqlnicks.nick")) {
-            player.sendMessage(MessageUtil.getMessage("nick.no-permission"));
+
+        if (!sender.hasPermission("mysqlnicks.nick")) {
+            sender.sendMessage(MessageUtil.getMessage("nick.no-permission"));
             return true;
         }
 
         //The player who's nickname is been changed
         final Player target;
         String nickname;
-        if (args.length == 2 && player.hasPermission("mysqlnicks.staff")) {
+        if (args.length == 2 && sender.hasPermission("mysqlnicks.staff")) {
             target = Bukkit.getPlayer(args[0]);
             if (target == null) {
-                player.sendMessage(MessageUtil.getMessage("nick.invalid-player"));
+                sender.sendMessage(MessageUtil.getMessage("nick.invalid-player"));
                 return true;
             }
             nickname = args[1];
         } else {
-            target = player;
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(MessageUtil.getMessage("nick.no-console"));
+                return true;
+            }
+            target = ((Player) sender);
             nickname = args[0];
         }
 
@@ -51,49 +51,53 @@ public class NickCMD implements CommandExecutor {
             boolean includeColors = config.getBoolean("limit.include-color");
             int length = config.getInt("limit.length");
             String input = includeColors ? nickname : ChatColor.stripColor(MessageUtil.format(nickname));
-            if (input.length() > length && !player.hasPermission("mysqlnicks.bypass.limit")) {
-                player.sendMessage(MessageUtil.getMessage("nick.exceeds-limit"));
+            if (input.length() > length && !sender.hasPermission("mysqlnicks.bypass.limit")) {
+                sender.sendMessage(MessageUtil.getMessage("nick.exceeds-limit"));
                 return true;
             }
         }
 
         nickname = nickname.replace("§", "&");
-        if (!player.hasPermission("mysqlnicks.nick.color")) {
-            if (!player.hasPermission("mysqlnicks.nick.color.simple") && !MessageUtil.stripLegacy(nickname).equals(nickname)) {
-                player.sendMessage(MessageUtil.getMessage("nick.no-codes"));
+        if (!sender.hasPermission("mysqlnicks.nick.color")) {
+            if (!sender.hasPermission("mysqlnicks.nick.color.simple") && !MessageUtil.stripLegacy(nickname).equals(nickname)) {
+                sender.sendMessage(MessageUtil.getMessage("nick.no-codes"));
                 return true;
             }
-            if (!player.hasPermission("mysqlnicks.nick.color.hex") && !MessageUtil.stripHex(nickname).equals(nickname)) {
-                player.sendMessage(MessageUtil.getMessage("nick.no-hex"));
+            if (!sender.hasPermission("mysqlnicks.nick.color.hex") && !MessageUtil.stripHex(nickname).equals(nickname)) {
+                sender.sendMessage(MessageUtil.getMessage("nick.no-hex"));
                 return true;
             }
         }
-        if (!player.hasPermission("mysqlnicks.nick.format") && (nickname.contains("&l") ||
+        if (!sender.hasPermission("mysqlnicks.nick.format") && (nickname.contains("&l") ||
                 nickname.contains("&m") || nickname.contains("&n") || nickname.contains("&o") ||
                 nickname.contains("&r"))) {
-            player.sendMessage(MessageUtil.getMessage("nick.no-formatting"));
+            sender.sendMessage(MessageUtil.getMessage("nick.no-formatting"));
             return true;
         }
-        if (!player.hasPermission("mysqlnicks.nick.magic") && nickname.contains("&k")) {
-            player.sendMessage(MessageUtil.getMessage("nick.no-magic"));
+        if (!sender.hasPermission("mysqlnicks.nick.magic") && nickname.contains("&k")) {
+            sender.sendMessage(MessageUtil.getMessage("nick.no-magic"));
             return true;
         }
 
         if (nickname.equalsIgnoreCase("off"))
             nickname = null;
-
-        String path = "nick." + (target.equals(player) ? "" : "staff.");
+        final String path;
+        if (sender instanceof Player) {
+            path = "nick." + (target.equals(sender) ? "" : "staff.");
+        } else {
+            path = "nick.staff.";
+        }
         if (!DataUtil.setNickname(target.getUniqueId(), nickname)) {
-            player.sendMessage(MessageUtil.getMessage(path + "error",
+            sender.sendMessage(MessageUtil.getMessage(path + "error",
                     "%player%", target.getName()));
             return true;
         }
         if (nickname == null) {
-            player.sendMessage(MessageUtil.getMessage(path + "unset",
+            sender.sendMessage(MessageUtil.getMessage(path + "unset",
                     "%player%", target.getName()));
             return true;
         }
-        player.sendMessage(MessageUtil.getMessage(path + "set",
+        sender.sendMessage(MessageUtil.getMessage(path + "set",
                 "%player%", target.getName(),
                 "%nickname%", nickname));
         return true;
